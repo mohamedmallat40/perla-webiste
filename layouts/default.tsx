e4835b9a27449a3a0c700213e2a45443c6363a85
Mallat Mohamed <med.mallat.official@gmail.com>
2025-04-01 16:58:30 +0100

    first commit

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -20,11 +20,11 @@ export default function DefaultLayout({
         <Link
           isExternal
           className="flex items-center gap-1 text-current"
-          href="https://www.heroui.com"
-          title="heroui.com homepage"
+          href="https://www.perla-it.com"
+          title="perla-it.com homepage"
         >
           <span className="text-default-600">Powered by</span>
-          <p className="text-primary">HeroUI</p>
+          <p className="text-primary">Perla it</p>
         </Link>
       </footer>
     </div>
